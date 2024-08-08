e73b0e7066d51df9e8ec11311f3a1159a8fe89ff
bluis <bluisfernandes@gmail.com>
2024-08-08 15:48:23 -0500

    add install_packages

--- install_packages.sh
+++ install_packages.sh
@@ -2,21 +2,57 @@
 
 # Array of package names
 packages=(
+    "zsh"
     "bat"
-    "grex"
+    #"grex"
     "exa"
-    "dust"
+    #"dust"
     "ripgrep"  # 'rg' is installed as 'ripgrep'
     "fd-find"  # 'fd' is installed as 'fd-find'
     "neofetch"
 )
 
+# Function to check which packages are already installed
+check_installed_packages() {
+    installed=()
+    not_installed=()
+
+    for package in "${packages[@]}"; do
+        if dpkg -l | grep -q "^ii  $package"; then
+            installed+=("$package")
+        else
+            not_installed+=("$package")
+        fi
+    done
+
+    # Display the packages that are already installed
+    if [ ${#installed[@]} -gt 0 ]; then
+        echo "The following packages are already installed:"
+        for package in "${installed[@]}"; do
+            echo -e "  - \e[32m$package\e[0m"  # Green color for installed packages
+        done
+    else
+        echo "No packages are already installed."
+    fi
+
+    # Display the packages that will be installed
+    if [ ${#not_installed[@]} -gt 0 ]; then
+        echo "The following packages will be installed:"
+        for package in "${not_installed[@]}"; do
+            echo -e "  - \e[33m$package\e[0m"  # Yellow color for packages to be installed
+        done
+    else
+        echo "All specified packages are already installed. Nothing to do."
+    fi
+}
+
 # Function to display the packages to be installed and ask for confirmation
 ask_for_confirmation() {
-    echo "The following packages will be installed:"
-    for package in "${packages[@]}"; do
-        echo "  - $package"
-    done
+    check_installed_packages
+
+    if [ ${#not_installed[@]} -eq 0 ]; then
+        return 1  # Exit if no packages need to be installed
+    fi
 
     read -p "Do you want to proceed with the installation? (y/n): " answer
     case $answer in
@@ -37,7 +73,7 @@ ask_for_confirmation() {
 # Function to install packages
 install_packages() {
     echo "Starting installation..."
-    for package in "${packages[@]}"; do
+    for package in "${not_installed[@]}"; do
         echo -e "⬇️  Installing \e[33m$package\e[0m..."
         #sudo apt install "$package"
     done
